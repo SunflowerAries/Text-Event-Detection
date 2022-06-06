0d8e3315a9a5c340027076156176632e79f11f2c
yubinshi <tens444@163.com>
2022-06-06 16:50:30 +0800

    tutorial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
-import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class Main {
-    public static void main(String[] args) {
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        System.out.println("123");
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> input = env.socketTextStream("localhost", 6666);
+
+        DataStream<Tuple2<String, Integer>> result = input.flatMap(
+            new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                    String[] words = s.split(" ");
+                    for (String word: words) {
+                        collector.collect(new Tuple2<String, Integer>(word, 1));
+                    }
+                }
+            }
+        ).keyBy(0).sum(1);
+
+        result.print();
+
+        env.execute();
     }
 }
