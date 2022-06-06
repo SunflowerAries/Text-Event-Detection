7f260f612685c57fd6b586c4522651c29c014e46
yubinshi <tens444@163.com>
2022-06-06 16:33:40 +0800

    basic dependency

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        System.out.println("123");
+    }
+}
